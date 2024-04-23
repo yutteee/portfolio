92a7533cfdc8674215e03f55e0f45f9e41f23e07
yutteee <yusaku_nakamura_aa@icloud.com>
2024-04-24 01:15:37 +0900

    データ追加の仕組みができた

---
+++ src/data/history.ts
@@ -0,0 +1,20 @@
+export const histories = [
+  {
+    title: "合同会社KANNON",
+    description:
+      "フェアナビというアクセシビリティオーバーレイSaaSの開発をしています。フロントエンドを中心に、デザインの提案などもしながら業務に取り組んでいます。",
+    image: "/kannon.png",
+  },
+  {
+    title: "株式会社スタメン",
+    description:
+      "フロントエンドエンジニアとしてサマーインターンに参加しました。TUNAGというプロダクトのNext.jsのリプレイスを行いました。",
+    image: "/stmn.png",
+  },
+  {
+    title: "技育展2022",
+    description:
+      "プログラミング言語やフレームワークで戦うカードゲームであるCODE_DUELというプロダクトをチームで開発しました。ゲーム部門で優秀賞をいただくことができました。",
+    image: "/giikuten.png",
+  },
+];

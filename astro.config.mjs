5b8208337a6ab8f25112574a3ac0dc3fb868024a
yutteee <yusaku_nakamura_aa@icloud.com>
2024-07-10 19:53:14 +0900

    プロダクトページ完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import icon from "astro-icon"
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), icon()],
 });

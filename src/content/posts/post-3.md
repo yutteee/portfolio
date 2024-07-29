e008d104fcd0a0fa47b606f1123d27450da688d4
yutteee <yusaku_nakamura_aa@icloud.com>
2024-07-29 17:09:47 +0900

    ogp画像更新

--- src/content/posts/post-3.md
+++ src/content/posts/post-3.md
@@ -2,9 +2,6 @@
 title: 私の3番目のブログ記事
 author: Astro学習者
 description: "上手くいかないことがありましたが、コミュニティに質問して助けてもらいました！"
-image:
-    url: "https://docs.astro.build/assets/rays.webp"
-    alt: "Astroの光線のサムネイル。"
 pubDate: "2022-07-15"
 tags: ["astro", "公開学習", "後退", "コミュニティ"]
 ---
--- src/utils/CreateOgImage.tsx
+++ src/utils/CreateOgImage.tsx
@@ -8,36 +8,62 @@ export async function createOgImage(text: string) {
     const boldFont = await fs.readFile("public/ZenKakuGothicNew-Bold.ttf");
 
     const svg = await satori(
-        <div
-          style={{
-            display: "flex",
-            height: "100%",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundImage: "linear-gradient(90deg, #FFC121, #FF5E2C)",
-            fontFamily: "Zen Kaku Gothic New",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: "8px",
-              gap: "16px",
-              backgroundColor: "#FFFCF2",
-              color: "#484335",
-              width: "90%",
-              height: "85%",
-              padding: "32px 64px",
-            }}
-          >
-            <div style={{ display: "flex", fontSize: "48px", fontWeight: 700 }}>{text}</div>
-            <div style={{ display: "flex", fontSize: "32px" }}>yutteee</div>
-          </div>
-        </div>,
+      <div
+      style={{
+        backgroundColor: "#84c5ea",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "40px",
+        fontFamily: "Zen Kaku Gothic New",
+        color: "#041b47"
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#f0f0f0",
+          width: "400px",
+          height: "240px",
+          borderRadius: "12px",
+          marginTop: "64px",
+        }}
+      >
+      </div>
+      <div
+        style={{
+          width: "150px",
+          borderBottom: "28px solid #f0f0f0",
+          borderLeft: "8px solid transparent",
+          borderRight: "8px solid transparent"
+        }}
+      >
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          fontSize: "4rem",
+          fontWeight: 700,
+          padding: "0 64px"
+        }}
+      >
+        {text}
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "32px",
+          fontSize: "1.5rem",
+        }}
+      >
+        yutteee-portfolio
+      </div>
+    </div>,
         {
           width: 1200,
           height: 630,
